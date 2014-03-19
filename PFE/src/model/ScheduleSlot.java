778eecbdad5d9576bf15b01fd115ae94c7f82146
pjlacharite <pjlacharite@gmail.com>
2014-03-19 18:32:35 -0400

    Succesfully parsing schedule slots

--- PFE/src/model/ScheduleSlot.java
+++ PFE/src/model/ScheduleSlot.java
@@ -1,10 +1,75 @@
 package model;
 
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
 /**
  * Class representing a Schedule Slot
  * @author pjlacharite
  *
  */
+@Root
 public class ScheduleSlot {
+    @Attribute
+    private String serieId;
+    @Attribute
+    private String broadcasterId;
+    @Attribute
+    private String duration;
+    @Attribute
+    private String title;
+    @Attribute
+    private String episodeTitle;
+    @Attribute
+    private String airingTime;
+    @Attribute
+    private String source;
 
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public ScheduleSlot(){
+    }
+
+    public String getSerieId() {
+        return serieId;
+    }
+    public void setSerieId(String serieId) {
+        this.serieId = serieId;
+    }
+    public String getBroadcasterId() {
+        return broadcasterId;
+    }
+    public void setBroadcasterId(String broadcasterId) {
+        this.broadcasterId = broadcasterId;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getEpisodeTitle() {
+        return episodeTitle;
+    }
+    public void setEpisodeTitle(String episodeTitle) {
+        this.episodeTitle = episodeTitle;
+    }
+    public String getAiringTime() {
+        return airingTime;
+    }
+    public void setAiringTime(String airingTime) {
+        this.airingTime = airingTime;
+    }
 }
--- WS/src/parsers/ROVIScheduleParser.java
+++ WS/src/parsers/ROVIScheduleParser.java
@@ -1,7 +1,46 @@
 package parsers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.ScheduleSlot;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class ROVIScheduleParser {
-    public String parse(String json){
-        return json;
+    public List<ScheduleSlot> parse(String json){
+        List<ScheduleSlot> scheduleSlots = new ArrayList<ScheduleSlot>();
+        JSONObject jsonObject1;
+        JSONObject jsonObject2;
+        JSONArray jsonArray;
+        try{
+            jsonObject1 = (JSONObject) new JSONParser().parse(json);
+            jsonObject2 = (JSONObject) jsonObject1.get("ProgramDetailsResult");
+            jsonObject1 = (JSONObject) jsonObject2.get("Schedule");
+            jsonArray = (JSONArray) jsonObject1.get("Airings");
+            for (int i = 0; i < jsonArray.size(); i++){
+                ScheduleSlot scheduleSlot = new ScheduleSlot();
+                scheduleSlot.setAiringTime(((JSONObject)jsonArray.get(i)).get("AiringTime").toString());
+                scheduleSlot.setBroadcasterId(((JSONObject)jsonArray.get(i)).get("ServiceId").toString());
+                scheduleSlot.setSerieId(((JSONObject)jsonArray.get(i)).get("SeriesId").toString());
+                scheduleSlot.setDuration(((JSONObject)jsonArray.get(i)).get("Duration").toString());
+                scheduleSlot.setTitle(((JSONObject)jsonArray.get(i)).get("Title").toString());
+                scheduleSlot.setEpisodeTitle(((JSONObject)jsonArray.get(i)).get("EpisodeTitle").toString());
+                scheduleSlot.setSource(((JSONObject)jsonArray.get(i)).get("SourceDisplayName").toString());
+                System.out.println(scheduleSlot.getAiringTime());
+                System.out.println(scheduleSlot.getBroadcasterId());
+                System.out.println(scheduleSlot.getDuration());
+                System.out.println(scheduleSlot.getEpisodeTitle());
+                System.out.println(scheduleSlot.getSerieId());
+                System.out.println(scheduleSlot.getSource());
+                System.out.println(scheduleSlot.getTitle());
+                scheduleSlots.add(scheduleSlot);
+            }
+        } catch (org.json.simple.parser.ParseException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+        return scheduleSlots;
     }
 }
